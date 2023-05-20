3869ea2b274dcb214fa6a50b6c7a7f01a4e1ef18
Eliezer.DV2 <62904599+Eliezer-D@users.noreply.github.com>
2023-05-20 18:24:32 +0100

    Update Checkpoint-file-handling.py

--- Checkpoint-file-handling.py
+++ Checkpoint-file-handling.py
@@ -1,15 +1,10 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[14]:
-
+#Checkpoint sur la lecture et manipulation de fichier en pyton avec Numpy
 
 import numpy as np
 
 # Ouverture du fichier
+#le fichier data(Lending-Company-Saving.csv) se retrouve dans le meme dossier source que le fichier python qui contient le code
 file = open("Lending-Company-Saving.csv", "r")
-#print(file.read())
-#file.close()
 
 # Lecture des données avec la fonction genfromtxt
 data = np.genfromtxt(file, delimiter=",")
@@ -28,10 +23,3 @@ std_amount = np.nanstd(amounts)
 print("Moyenne des montants de prêt :", mean_amount)
 print("Médiane des montants de prêt :", median_amount)
 print("Écart type des montants de prêt :", std_amount)
-
-
-# In[ ]:
-
-
-
-
